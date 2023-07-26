c91655a095055bb862b3dee0fe1bd9c3050bc665
malaikaikartel <50528583+malaikaikartel@users.noreply.github.com>
2023-07-26 22:18:16 +0700

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -4,8 +4,14 @@ export default function Head() {
     <>
       <title>Red Frame</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6389784149267865"
-     crossorigin="anonymous"/> 
+     <Script
+   id="Adsense-id"
+   // your tag google publisher
+   data-ad-client="ca-pub-6389784149267865"
+   async="true"
+   strategy="beforeInteractive"
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+/>
 <link rel="icon" href="/favicon.ico" />
     </>
   );
